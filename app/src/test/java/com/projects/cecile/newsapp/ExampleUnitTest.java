ef31ba693c05a8d9e76b953c30657e81b075dfd3
CecileJiaqiWang <jw81@rice.edu>
2018-08-05 18:56:46 -0500

    build settings

--- app/src/test/java/com/projects/cecile/newsapp/ExampleUnitTest.java
+++ app/src/test/java/com/projects/cecile/newsapp/ExampleUnitTest.java
@@ -2,7 +2,7 @@ package com.projects.cecile.newsapp;
 
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
